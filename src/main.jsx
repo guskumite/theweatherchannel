42d5b75ac59ae3ff906692f1cc4f18e4ebe374b5
guskumite <gmolanoruiz2000@gmail.com>
2023-08-08 23:20:24 -0500

    version 1.0.7

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { useState, useEffect } from "react";
 
 const [countryName, setCountryName] = useState(null);
 
